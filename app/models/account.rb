557f719a6c4f6fcd1470cbcf01acdb3dfa44bb99
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-22 16:30:21 +0400

    Add associations for Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,6 +12,25 @@ class Account < ApplicationRecord
   }
 
   belongs_to :user
+  
   has_many :account_interest
-  has_many :interest, through: :account_interest
+  has_many :interests, through: :account_interest
+
+  has_many :group_participant
+  has_many :groups, through: :group_participant 
+
+  has_many :comments, class_name: 'Comment', foreign_key: :author_comment_id, dependent: :destroy
+  has_many :posts, class_name: 'Post', foreign_key: :author_post_id, dependent: :destroy
+
+  has_many :sender_invite, class_name: 'Invite', foreign_key: :sender_invite_id, dependent: :destroy
+  has_many :receiver_invite, class_name: 'Invite', foreign_key: :receiver_invite_id, dependent: :destroy
+
+  has_many :f_partner_friendship, class_name: 'Friendship', foreign_key: :f_partner_friendship_id, dependent: :destroy
+  has_many :s_partner_friendship, class_name: 'Friendship', foreign_key: :s_partner_friendship_id, dependent: :destroy
+
+  has_many :sender_message, class_name: 'Message', foreign_key: :sender_message_id, dependent: :destroy
+  has_many :reciepient_message, class_name: 'Message', foreign_key: :reciepient_message_id, dependent: :destroy
+
+  has_many :f_partner_conversation, class_name: 'Conversation', foreign_key: :f_partner_conversation_id, through: :sender_message, dependent: :destroy
+  has_many :s_partner_conversation, class_name: 'Conversation', foreign_key: :s_partner_conversation_id, through: :reciepient_message, dependent: :destroy
 end
