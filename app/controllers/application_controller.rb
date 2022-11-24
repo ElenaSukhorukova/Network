ff30a5105148c2fb68211d2e7e2f8e25b901ae40
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:26:31 +0400

    Configure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :i18n_model_name
+
+  add_flash_types :info, :danger, :warning, :success, :alert, :notice
+  
+  private
+    
+    def i18n_model_name(model)
+      model.model_name.human
+    end
 end
