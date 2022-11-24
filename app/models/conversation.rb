32565b9d53ef5eb6cb96be104c7b6a7c3243cb95
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:29:23 +0400

    Update association in conversation's model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
   has_many :messages
 
-  has_many :f_partner_conversation, class_name: "Account", through: :message, optional: true
-  has_many :f_partner_conversation, class_name: "Account", through: :message, optional: true
+  has_many :f_partner_conversation, class_name: "Account", through: :message
+  has_many :f_partner_conversation, class_name: "Account", through: :message
 end
