2bf764921eee0c74ff2664ad2d6f258ed63a33b1
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-24 23:33:26 +0400

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,38 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/about'
-  get 'static_pages/rules'
-  get 'static_pages/contacts'
 
-  root to: "static_pages#about"
+  devise_scope :user do  
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+
+  get 'about', to: 'static_pages#about'
+  get 'rules', to: 'static_pages#rules'
+  get 'contacts', to: 'static_pages#contacts'
+
+  root to: "posts#index"
+  resources :posts, only: %i[index]
+
+  shallow do 
+    resources :users do 
+      resource :account
+    end
+    resources :accounts do
+      resources :posts
+      resources :conversations
+      resources :friendships
+      resources :groups
+      resources :invites, only: %i[create destroy]
+    end
+    resources :posts do
+      resources :comments
+    end
+    resources :friendships do 
+      resource :invite
+    end
+    resources :conversation do 
+      resources :messages
+    end
+  end
 end
