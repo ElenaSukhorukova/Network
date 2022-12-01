67bf5ab537f707292109084c4cc6539c20201059
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:21:07 +0400

    Add a method and a macket validation for post's model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
+  # validates :body, presence: true, if: :custom_validation
+
   belongs_to :author_post, class_name: "Account", optional: true
   has_many :comments, as: :commentable, dependent: :destroy
+
+  # def custom_validation
+  #   !images.attached?
+  # end
+  def formatted_created_at
+    created_at.strftime('%Y-%m-%d')
+  end
 end
