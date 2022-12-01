778ad950aa7247c4dd3397a75bac1ab6ae61e800
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:19:56 +0400

    Add methods fo invite's model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,24 @@
 class Invite < ApplicationRecord
+  VALID_CONFIRMED = ['not', 'yes', 'canceled']
+  validates :confirmed, inclusion: { in: VALID_CONFIRMED }
+
   has_one :friendship
   belongs_to :sender_invite, class_name: "Account", optional: true
   belongs_to :receiver_invite, class_name: "Account", optional: true
+
+  def self.find_invites_sender(account_id)
+    invites = []
+    self.all.collect do |invite|
+      invites << invite if invite.sender_invite_id == account_id && invite.confirmed != 'yes'
+    end
+    invites
+  end
+
+  def self.find_invites_receiver(account_id)
+    invites = []
+    self.all.collect do |invite|
+      invites << invite if invite.receiver_invite_id == account_id && invite.confirmed != 'yes'
+    end
+    invites
+  end
 end
