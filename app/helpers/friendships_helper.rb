76e991c6815d3d233307f55801f54368df6686e3
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:11:41 +0400

    Add friendships' helper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,8 @@
 module FriendshipsHelper
+  def check_friendship?(sender_id, recipient_id)
+   friendship = Friendship.find_by(f_partner_friendship_id: sender_id, s_partner_friendship_id: recipient_id) ||
+   Friendship.find_by(s_partner_friendship_id: sender_id, f_partner_friendship_id: recipient_id)
+    
+    true if friendship
+  end
 end
