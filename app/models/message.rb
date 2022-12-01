7fefd09593849665f2f2132e1d20559183349978
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:20:21 +0400

    Add a validation for message's model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
+  validates :body, presence: true
+
   belongs_to :conversation
 
   belongs_to :sender_message, class_name: "Account", optional: true
-  belongs_to :reciepient_message, class_name: "Account", optional: true
+  belongs_to :recipient_message, class_name: "Account", optional: true
 end
