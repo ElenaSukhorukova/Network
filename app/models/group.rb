e7d853f805ba6d00fdc7c09835d19830278bcbf7
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:17:24 +0400

    Add validations for group and change associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,14 @@
 class Group < ApplicationRecord
   VALID_VISIBILITY = ['everybody', 'participants']
 
+  validates :name_grou,  length: { within: 2..50 }
+  
   has_many :group_participant
   has_many :accounts, through: :group_participants
 
-  has_many :group_interests
+  has_many :group_interest
   has_many :interests, through: :group_interest
+  accepts_nested_attributes_for :interests, allow_destroy: true
+
+  belongs_to :group_creator, class_name: 'Acccount', optional: true
 end
