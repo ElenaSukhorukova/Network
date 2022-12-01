76f6cb62cc0475db89ef52d4549c3a73879a30a0
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:11:07 +0400

    Configure posts' controller

--- app/controllers/accounts/posts_controller.rb
+++ app/controllers/accounts/posts_controller.rb
@@ -1,15 +1,7 @@
-class PostsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index]
+class Accounts::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :define_account
   before_action :define_post, except: %i[index new create]
-  before_action :define_account, only: %i[new create index]
-
-  def show
-    @comments = @post.comments
-  end
-
-  def index
-    @posts = Post.where(place: 'content').order(created_at: :desc)
-  end
 
   def new 
     @post = @account.posts.build
@@ -19,7 +11,7 @@ class PostsController < ApplicationController
     @post = @account.posts.build(post_params)
     
     if @post.save     
-      redirect_to post_path(@post), 
+      redirect_to account_path(@account), 
         success: I18n.t('flash.new', model: i18n_model_name(@post).downcase)
     else
       render :new, status: :unprocessable_entity
@@ -31,7 +23,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to post_path(@post),
+      redirect_to account_path(@account),
         success: I18n.t('flash.update', model: i18n_model_name(@post).downcase)
     else
       render :edit, status: :unprocessable_entity
@@ -40,12 +32,13 @@ class PostsController < ApplicationController
 
   def destroy
     if @post.destroy
-      redirect_to posts_path, 
+      redirect_to account_path(@account),
         success: I18n.t('flash.destroy', model: i18n_model_name(@post).downcase)
     end
   end
 
   private
+
     def define_post
       @post = Post.find(params[:id])
     end
