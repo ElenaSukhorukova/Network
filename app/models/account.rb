5406791c25acc68c4bef3bca615cb47dc630783b
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:13:45 +0400

    Update accounts' validations and associations

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,20 +6,30 @@ class Account < ApplicationRecord
   STATE = ['online', 'ofline']
 
   validates :user_name, presence: true, length: { within: 2..50 }
-  validates :gender, inclusion: { in: VALID_GENDERS }
-  validates :date_birthday, format: { with: /\d{4}\-\d{2}\-\d{2}/,
-    message: "a format of date is allowed only xx.xx.xxxx" }
-  validates :date_birthday, comparison: { greater_than: Date.parse('1900-01-01'), less_than: Date.current, 
-    message: 'You should be little elder then a current date and younger then 1900-01-01.'
-  }
+  validates :country, :gender, :date_birthday, presence: true
+  validate :validate_age
+
+  def validate_age
+    if date_birthday.present? && date_birthday > 18.years.ago
+      errors.add(date_birthday, 'You should be over 18 years old.')
+    end
+  end
+
+  has_many :f_partner_conversation, class_name: 'Conversation', foreign_key: :f_partner_conversation_id, through: :sender_message, dependent: :destroy
+  has_many :s_partner_conversation, class_name: 'Conversation', foreign_key: :s_partner_conversation_id, through: :reciepient_message, dependent: :destroy
+  
+  has_many :sender_message, class_name: 'Message', foreign_key: :sender_message_id, dependent: :destroy
+  has_many :recipient_message, class_name: 'Message', foreign_key: :recipient_message_id, dependent: :destroy
 
   belongs_to :user
   
   has_many :account_interest
   has_many :interests, through: :account_interest
+  accepts_nested_attributes_for :interests, allow_destroy: true
 
   has_many :group_participant
   has_many :groups, through: :group_participant 
+  has_many :group_creator, class_name: 'Group', foreign_key: :group_creator_id, dependent: :destroy
 
   has_many :comments, class_name: 'Comment', foreign_key: :author_comment_id, dependent: :destroy
   has_many :posts, class_name: 'Post', foreign_key: :author_post_id, dependent: :destroy
@@ -29,10 +39,7 @@ class Account < ApplicationRecord
 
   has_many :f_partner_friendship, class_name: 'Friendship', foreign_key: :f_partner_friendship_id, dependent: :destroy
   has_many :s_partner_friendship, class_name: 'Friendship', foreign_key: :s_partner_friendship_id, dependent: :destroy
-
-  has_many :sender_message, class_name: 'Message', foreign_key: :sender_message_id, dependent: :destroy
-  has_many :reciepient_message, class_name: 'Message', foreign_key: :reciepient_message_id, dependent: :destroy
-
-  has_many :f_partner_conversation, class_name: 'Conversation', foreign_key: :f_partner_conversation_id, through: :sender_message, dependent: :destroy
-  has_many :s_partner_conversation, class_name: 'Conversation', foreign_key: :s_partner_conversation_id, through: :reciepient_message, dependent: :destroy
+ 
+  # to show account except current
+  scope :except_current_account, ->(account) { where.not(id: account) }
 end
