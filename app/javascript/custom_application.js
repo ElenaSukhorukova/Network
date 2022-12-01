8a6c468dcd819956dc8bcfbb4aa485745417a7c9
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:07:02 +0400

    Add bootstrap and custom styles

--- app/javascript/custom_application.js
+++ app/javascript/custom_application.js
@@ -1,5 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
-//= require country_state_select
-import 'bootstrap'
+import "./controllers"
+import * as bootstrap from "bootstrap"
