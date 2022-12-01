fb3273d956071be6fe852968e7e9345e3445ec3e
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:16:50 +0400

    Add mthod for accounts' friends

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,12 @@ class Friendship < ApplicationRecord
   belongs_to :invite
   belongs_to :f_partner_friendship, class_name: "Account", optional: true
   belongs_to :s_partner_friendship, class_name: "Account", optional: true
+
+  def self.account_friends(account_id)
+    friends = []
+    self.all.collect do |friendship|
+      friends << friendship if [ friendship.f_partner_friendship_id, friendship.s_partner_friendship_id].include?(account_id)
+    end
+    friends
+  end
 end
