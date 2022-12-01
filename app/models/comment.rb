ac7536461a78e9a54fe6b71e55df2bfcf162d7fd
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-01 18:15:53 +0400

    Add validation for comment's model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true
+
   belongs_to :author_comment, class_name: "Account", optional: true
   belongs_to :commentable, polymorphic: true
 end
