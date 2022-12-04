1e92bc424714a75a5e20caaf9a460e68be9bbec1
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-04 12:31:04 +0400

    Add ErrorHandling. Edit application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  helper_method :i18n_model_name
+  include ErrorHandling
+  include ApplicationHelper
   
   add_flash_types :info, :danger, :warning, :success, :alert, :notice
   
-  private
-    
-    def i18n_model_name(model, count: 1)
-      model.model_name.human(count: count)
-    end
 end
