97ac5b07f42862f998beef8c1985f9675c2a9c59
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:07:46 +0400

    Update a method find_friends in friendship's model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,13 +1,10 @@
-class Friendship < ApplicationRecord
+class Friendship < ApplicationRecord 
   belongs_to :invite
   belongs_to :f_partner_friendship, class_name: "Account", optional: true
   belongs_to :s_partner_friendship, class_name: "Account", optional: true
 
-  def self.account_friends(account_id)
-    friends = []
-    self.all.collect do |friendship|
-      friends << friendship if [ friendship.f_partner_friendship_id, friendship.s_partner_friendship_id].include?(account_id)
-    end
-    friends
+
+  def self.user_friends(account)
+    where(f_partner_friendship: account).or(Friendship.where(s_partner_friendship: account))
   end
 end
