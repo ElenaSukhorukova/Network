da15e84205979b7589c884d98cc61d2596f6009e
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 14:57:45 +0400

    Update comments' controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :authenticate_user!
   before_action :define_variables!
   before_action :define_comment!, except: %i[create]
   helper_method :commentable_path
   
   def create
-    @comment = @commentable.comments.build(comment_params)
+    @comment = @commentable.comments.build comment_params
     @comment.author_comment = @account
 
     if @comment.save
@@ -22,7 +23,7 @@ class CommentsController < ApplicationController
 
   def update 
     if @comment.author_comment == @account
-      if @comment.update(comment_params)
+      if @comment.update comment_params
         redirect_to commentable_path(@comment),
           success: I18n.t('flash.update', model: i18n_model_name(@comment).downcase)
       else
@@ -33,8 +34,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @post = @comment.commentable
-
     if @comment.destroy
       redirect_to commentable_path(@comment), 
         success: I18n.t('flash.destroy', model: i18n_model_name(@comment).downcase)
@@ -43,23 +42,25 @@ class CommentsController < ApplicationController
 
   private
     
-    def define_comment!
-      @comment = Comment.find(params[:id])
-    end
+  def define_comment!
+    @comment = Comment.find params[:id]
+  end
 
-    def define_variables!
-      @account = Account.find_by(user_id: current_user.id)
+  def define_variables!
+    @account = Account.find_by user_id: current_user.id
 
-      # set commentable
-      resource, id = request.path.split('/')[1, 2]  
-      @commentable = resource.singularize.classify.constantize.find(id)
-    end
+    # set commentable
+    resource, id = request.path.split('/')[1, 2]  
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
 
-    def commentable_path(comment)
-      "/#{comment.commentable_type.downcase.pluralize}/#{comment.commentable_id}"
-    end
+  def commentable_path(comment)
+    return post_path(comment.commentable, anchor: dom_id(comment)) if comment.commentable_type == 'Post'
+    return content_path(comment.commentable, anchor: dom_id(comment)) if comment.commentable_type == 'Content'
+  end
+  helper_method :commentable_path
 
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
