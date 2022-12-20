59bc610442775544c24979520c859dc63d5eb0fb
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:04:37 +0400

    Add invite helpers

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,2 +1,11 @@
 module InvitesHelper
+  def find_account_name(invite)
+    return account = invite.sender_invite == current_user.account ? invite.receiver_invite :
+                                                                    invite.sender_invite
+  end
+
+  def check_invite?(sender_invite, receiver_invite)
+    return  true if Invite.find_by(sender_invite: sender_invite, receiver_invite: receiver_invite, confirmed: 'not') ||
+                    Invite.find_by(sender_invite: receiver_invite, receiver_invite: sender_invite, confirmed: 'not')
+  end
 end
