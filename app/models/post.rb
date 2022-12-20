2fbc183c59b2266005dd592112529715f38e4f51
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:09:11 +0400

    Update post's model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
-  include DateTimeShow
   # validates :body, presence: true, if: :custom_validation
 
   belongs_to :author_post, class_name: "Account", optional: true
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :order_desc, -> { order_desc }, class_name: 'Comment'
 
   # def custom_validation
   #   !images.attached?
-  # end
+  # end 
 end
