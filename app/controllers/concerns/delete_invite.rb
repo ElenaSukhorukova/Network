9d951394cd4422dc96626d034f60bc50e05ef07a
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:01:26 +0400

    Add concern to delete invites

---
+++ app/controllers/concerns/delete_invite.rb
@@ -0,0 +1,14 @@
+module DeleteInvite
+  extend ActiveSupport::Concern
+
+  included do 
+    private 
+
+    def delete_invite 
+      invites = Invite.where(confirmed: 'yes')
+      invites.each do |invite|
+        invite.destroy unless Friendship.find_by(invite_id: invite.id)
+      end
+    end
+  end
+end
