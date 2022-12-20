93d22704cb0cf118f2d4c5696ecbef9d0d754322
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:08:04 +0400

    Update group's model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,15 +1,20 @@
 class Group < ApplicationRecord
   VALID_VISIBILITY = ['everybody', 'participants']
 
-  validates :name_group, uniqueness: true, length: { within: 2..50 }
+  validates :name_group, presence: true, uniqueness: true, length: { within: 2..50 }
+  validates :description, presence: true, length: { minimum: 2 }
+
   validates :visibility, inclusion: { in: VALID_VISIBILITY }
  
-  has_many :group_participant
+  has_many :group_participants
   has_many :accounts, through: :group_participants
 
-  has_many :group_interest
-  has_many :interests, through: :group_interest
-  accepts_nested_attributes_for :interests, allow_destroy: true
+  has_many :group_hobbies
+  has_many :hobbies, through: :group_hobbies
+  accepts_nested_attributes_for :hobbies, allow_destroy: true, update_only: true
 
   belongs_to :group_creator, class_name: 'Account', optional: true
+  has_many :group_contents, dependent: :destroy
+
+  has_many :contents
 end
