d90dc8e082e96283e8d24bab254333d19eaaf153
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:06:09 +0400

    Create content's model

---
+++ app/models/content.rb
@@ -0,0 +1,7 @@
+class Content < ApplicationRecord
+  belongs_to :group
+  belongs_to :account
+
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :order_desc, -> { order_desc }, class_name: 'Comment'
+end
