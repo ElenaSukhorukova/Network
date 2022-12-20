f04de80860af6aef4f91d261ba46628774bc9dfc
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:03:40 +0400

    Add helpers for conversations

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,6 @@
 module ConversationsHelper
+  def find_interlocutor(conversation)
+    conversation.f_partner_conversation == current_user.account ? Account.find(conversation.s_partner_conversation_id).decorate :
+                                                                  Account.find(conversation.f_partner_conversation_id).decorate
+  end
 end
