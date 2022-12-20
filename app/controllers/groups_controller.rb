506d38d130c86f3ee6aa31e92c1ed0f24480a748
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 14:58:36 +0400

    Create groups'controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,20 +4,34 @@ class GroupsController < ApplicationController
   before_action :define_group!, except: %i[index new create]
 
   def index
-    @created_groups = Group.where(group_creator_id: @account.id)
-    @participation_groups = @account.groups.where.not(group_creator_id: @account.id)
+    @pagy, @all_groups = pagy Group.order(:name_group), items: 5
+    @all_groups = @all_groups.decorate
+  end
+
+  def your_groups
+    @pagy, @created_groups = pagy Group.where(group_creator: @account).order(:name_group), items: 5
+    @created_groups = @created_groups.decorate
+  end
+
+  def participation_groups
+    # @pagy, @participation_groups = pagy @account.groups.where.not(group_creator_id: @account.id), items: 5
+    # @participation_groups = @participation_groups.decorate
   end
 
   def show
+    @hobby = Hobby.new
+    @pagy, @contents = pagy @group.contents.order(created_at: :desc), items: 5
+    @contents = @contents.decorate
+    @participants = @group.accounts
   end
 
   def new
     @group = @account.groups.build
-    3.times { @group.interests.build }
+    3.times { @group.hobbies.build }
   end
   
   def create
-    @group = @account.groups.build(group_params)
+    @group = @account.groups.build group_params
     if @group.save
       redirect_to group_path(@group), 
       success: I18n.t('flash.new', model: i18n_model_name(@group).downcase)
@@ -49,18 +63,23 @@ class GroupsController < ApplicationController
     end
   end
 
-  private
-    
-    def define_account!
-      @account = Account.find_by(user_id: current_user.id)
-    end
+  private   
+  def define_account!
+    @account = Account.find_by user_id: current_user.id
+  end
 
-    def define_group!
-      @group = Group.find(params[:id])
-    end
+  def define_group!
+    @group = Group.find(params[:id]).decorate
+  end
 
-    def group_params
-      params.require(:group).permit(:name_group, :description, :visibility,
-        interests_attributes: [:id, :name_interest, :_destroy])
-    end
+  def become_group_participant
+    @account = Account.find_by user_id: current_user.id
+    @group.accounts << @account unless @group.accounts.exists?(@account.id)
+  end
+  helper_method :become_group_participant
+
+  def group_params
+    params.require(:group).permit(:name_group, :description, :visibility,
+                                  hobbies_attributes: [:id, :hobby_name, :_destroy])
+   end
 end
