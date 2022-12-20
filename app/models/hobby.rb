5816aa96476b4e8ee30aa3b9670191b3a3df659f
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:08:21 +0400

    Add hobby's model

---
+++ app/models/hobby.rb
@@ -0,0 +1,15 @@
+class Hobby < ApplicationRecord
+  validates :hobby_name, presence: true, length: { within: 3..50 }
+  
+  has_many :account_hobbies
+  has_many :account, through: :account_hobbies
+
+  has_many :group_hobbies
+  has_many :group, through: :group_hobbies
+
+  def self.new_hobby(params)
+    hobby = self.find_by(hobby_name: params[:hobby_name])
+
+    hobby ||= new(hobby_name: params[:hobby_name])
+  end
+end
