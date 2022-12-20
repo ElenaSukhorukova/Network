2a38ee74d3fff392ad21034c3452d1a840d7ad1d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 14:59:00 +0400

    Update invites' controllers

--- app/controllers/accounts/invites_controller.rb
+++ app/controllers/accounts/invites_controller.rb
@@ -1,20 +1,23 @@
 class Accounts::InvitesController < ApplicationController
+  include InvitesHelper
   before_action :authenticate_user!
   before_action :define_accounts!
 
   def create
-    @invite = Invite.new(sender_invite_id: @sender.id, receiver_invite_id: @receiver.id)
+    unless check_invite?(@sender.id, @receiver.id)
+      @invite = Invite.new sender_invite_id: @sender.id, receiver_invite_id: @receiver.id
 
-    if @invite.save
-      redirect_to account_path(@receiver),
-        success: I18n.t('flash.send', model: i18n_model_name(@invite).downcase)
+      if @invite.save
+        redirect_to account_path(@receiver),
+          success: I18n.t('flash.send', model: i18n_model_name(@invite).downcase)
+      end
     end
   end
 
   def update
-    @invite = Invite.find(params[:id])
-    @account = Account.find(params[:account_id])
-    if @invite.update(invite_params)
+    @invite = Invite.find params[:id]
+    @account = Account.find params[:account_id] 
+    if @invite.update invite_params
       redirect_to account_path(@account),
       success: I18n.t('flash.cancel', model: i18n_model_name(@invite).downcase)
     end
@@ -23,8 +26,8 @@ class Accounts::InvitesController < ApplicationController
   private
 
   def define_accounts!
-    @sender = Account.find_by(user_id: current_user.id)
-    @receiver = Account.find(params[:account_id])
+    @sender = Account.find_by user_id: current_user.id
+    @receiver = Account.find params[:account_id]
   end
 
   def invite_params
--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,8 +2,8 @@ class InvitesController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    @invite = Invite.find(params[:id])
-    if @invite.update(invite_params)
+    @invite = Invite.find params[:id]
+    if @invite.update invite_params
       redirect_to account_friendships_path(current_user.account),
         success: I18n.t('flash.cancel', model: i18n_model_name(@invite).downcase)
     end
@@ -11,7 +11,7 @@ class InvitesController < ApplicationController
 
   private
     
-    def invite_params
-      params.require(:invite).permit(:confirmed)
-    end
+  def invite_params
+    params.require(:invite).permit(:confirmed)
+  end
 end
