7c6543d556b969c2fb0136379d1e9bb1366372dc
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:00:53 +0400

    Create contents' controller

---
+++ app/controllers/groups/contents_controller.rb
@@ -0,0 +1,66 @@
+class Groups::ContentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :define_content!, except: %i[index new create]
+  before_action :define_group!, only: %i[new create]
+  before_action :define_account!
+
+  def show
+    @group = @content.group
+    @pagy_comment, @comments = pagy @content.comments.order_desc, items: 4, page_param: :pagy_comment
+    @comments = @comments.decorate
+  end
+
+  def new 
+    @content = @group.contents.build
+  end
+
+  def create
+    @content = @group.contents.build content_params
+    @content.account = @account
+    if @content.save
+      redirect_to content_path(@content), 
+        success: I18n.t('flash.new', model: i18n_model_name(@content).downcase)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @account == @content.account
+      if @content.update content_params
+        redirect_to content_path(@content),
+          success: I18n.t('flash.update', model: i18n_model_name(@content).downcase)
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def destroy
+    if @content.destroy
+      redirect_to group_path(@group), 
+        success: I18n.t('flash.destroy', model: i18n_model_name(@content).downcase)
+    end
+  end
+
+  private
+  
+  def define_content!
+    @content = Content.find(params[:id]).decorate
+  end
+
+  def define_group!
+    @group = Group.find params[:group_id]
+  end
+  
+  def define_account!
+    @account = Account.find_by user_id: current_user.id
+  end
+
+  def content_params
+    params.require(:content).permit(:body)
+  end
+end
