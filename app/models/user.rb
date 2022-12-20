0d1c0995a24e6277d51e5739a6963a152b6ff477
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:09:25 +0400

    Update user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :account
+  has_one :account, dependent: :destroy
 end
