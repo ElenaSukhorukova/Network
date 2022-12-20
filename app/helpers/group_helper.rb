ca52b8b7a22a2f47f80dd790cb3b81bf29b1d91e
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:04:17 +0400

    Add helpers for groups

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -1,2 +1,14 @@
 module GroupHelper
+  def group_page_name(page_name = '')
+    render partial: 'navigation', locals: { page_name: page_name }
+  end
+
+  def link_nav(title, url, options = {})
+    page_name = options.delete :page_name
+    css_text = page_name == title ? "btn btn-dark my-2 btn-sm" : "btn btn-outline-dark my-2 btn-sm"
+    options[:class] = options[:class] ? (options[:class] + ' ' + css_text) : 
+                                        css_text
+    
+    link_to title, url, options
+  end
 end
