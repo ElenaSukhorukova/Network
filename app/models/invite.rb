045ecf2388f48031bde6ae17c328b8bf41d3e73c
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-20 15:08:30 +0400

    Update invite's model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -3,22 +3,15 @@ class Invite < ApplicationRecord
   validates :confirmed, inclusion: { in: VALID_CONFIRMED }
 
   has_one :friendship
+  
   belongs_to :sender_invite, class_name: "Account", optional: true
   belongs_to :receiver_invite, class_name: "Account", optional: true
 
-  def self.find_invites_sender(account_id)
-    invites = []
-    self.all.collect do |invite|
-      invites << invite if invite.sender_invite_id == account_id && invite.confirmed != 'yes'
-    end
-    invites
-  end
+  def self.account_sender_invites(account)
+    where(sender_invite: account).where.not(confirmed: 'yes')
+  end 
 
-  def self.find_invites_receiver(account_id)
-    invites = []
-    self.all.collect do |invite|
-      invites << invite if invite.receiver_invite_id == account_id && invite.confirmed != 'yes'
-    end
-    invites
-  end
+  def self.account_receiver_invites(account)
+    where(receiver_invite: account).where.not(confirmed: 'yes')
+  end 
 end
