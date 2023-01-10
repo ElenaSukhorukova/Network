dc04c1a2f28c076450057e47ce00e4d914fcb439
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-10 22:58:34 +0400

    Fix errors in account's model

--- app/models/account.rb
+++ app/models/account.rb
@@ -23,42 +23,40 @@ class Account < ApplicationRecord
   # to show account except current
   scope :except_current_account, ->(account) { where.not(id: account).order(:user_name) }
 
-  has_many :sender_message, class_name: 'Message',
-                            foreign_key: :sender_message_id,
-                            inverse_of: :sender_message, dependent: :destroy
+  has_many :sender_messages, class_name: 'Message',
+                             foreign_key: :sender_message_id,
+                             inverse_of: :sender_message, dependent: :destroy
 
-  has_many :recipient_message, class_name: 'Message',
-                               foreign_key: :recipient_message_id,
-                               inverse_of: :recipient_message, dependent: :destroy
+  has_many :recipient_messages, class_name: 'Message',
+                                foreign_key: :recipient_message_id,
+                                inverse_of: :recipient_message, dependent: :destroy
 
-  has_many :f_partner_conversation, class_name: 'Conversation',
-                                    foreign_key: :f_partner_conversation_id,
-                                    through: :sender_message, source: :sender_message,
-                                    inverse_of: :f_partner_conversation, dependent: :destroy
+  has_many :f_partner_conversations, class_name: 'Conversation',
+                                     foreign_key: :f_partner_conversation_id,
+                                     through: :sender_messages, source: :sender_message,
+                                     dependent: :destroy
 
-  has_many :s_partner_conversation, class_name: 'Conversation',
-                                    foreign_key: :s_partner_conversation_id,
-                                    through: :reciepient_message, source: :sender_message,
-                                    inverse_of: :s_partner_conversation, dependent: :destroy
-
+  has_many :s_partner_conversations, class_name: 'Conversation',
+                                     foreign_key: :s_partner_conversation_id,
+                                     through: :recipient_messages, source: :recipient_message,
+                                     dependent: :destroy
   belongs_to :user
-
   has_many :account_hobbies, dependent: :destroy
   has_many :hobbies, through: :account_hobbies
   accepts_nested_attributes_for :account_hobbies, allow_destroy: true
 
   has_many :group_participants, dependent: :destroy
-  has_many :groups, class_name: 'Group', foreign_key: :group_creator_id,
-                    inverse_of: :group_creator, dependent: :destroy
+  has_many :group_creator, class_name: 'Group', foreign_key: :group_creator_id,
+                          inverse_of: :group_creator, dependent: :destroy
 
-  has_many :comments, class_name: 'Comment',
-                      foreign_key: :author_comment_id,
-                      inverse_of: :author_comment, dependent: :destroy
+  has_many :author_comment, class_name: 'Comment',
+                           foreign_key: :author_comment_id,
+                           inverse_of: :author_comment, dependent: :destroy
 
-  has_many :posts, class_name: 'Post', foreign_key: :author_post_id,
+  has_many :author_posts, class_name: 'Post', foreign_key: :author_post_id,
                    inverse_of: :author_post, dependent: :destroy
 
-  has_many :group_contents, dependent: :destroy
+  has_many :contents, dependent: :destroy
 
   has_many :sender_invites, class_name: 'Invite',
                             foreign_key: :sender_invite_id,
@@ -70,9 +68,9 @@ class Account < ApplicationRecord
 
   has_many :f_partner_friendships, class_name: 'Friendship',
                                    foreign_key: :f_partner_friendship_id,
-                                   inverse_of: :friendship, dependent: :destroy
+                                   inverse_of: :f_partner_friendship, dependent: :destroy
 
   has_many :s_partner_friendships, class_name: 'Friendship',
                                    foreign_key: :s_partner_friendship_id,
-                                   inverse_of: :friendship, dependent: :destroy
+                                   inverse_of: :s_partner_friendship, dependent: :destroy
 end
