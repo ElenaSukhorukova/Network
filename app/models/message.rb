af91152bab55d996f44354304033809ec3c7b658
Elena Sukhorukova <suhorukova.el@gmail.com>
2023-01-10 22:59:41 +0400

    Edit message's model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,6 @@ class Message < ApplicationRecord
   validates :body, presence: true
 
   belongs_to :conversation
-
-  belongs_to :sender_message, class_name: 'Account', optional: true
-  belongs_to :recipient_message, class_name: 'Account', optional: true
+  belongs_to :sender_message
+  belongs_to :recipient_message
 end
